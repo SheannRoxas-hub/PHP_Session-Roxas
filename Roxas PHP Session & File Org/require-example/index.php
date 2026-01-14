3a0091a15b3544c4512a6dd713699e3c2ef964b0
SheannRoxas-hub <sheannvince.r@gmail.com>
2026-01-14 14:14:37 +0800

    Add files via upload

---
+++ Roxas PHP Session & File Org/require-example/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        require 'header.php'; // Loads header​
+        include 'greeting.php'; // Loads greeting message​
+        require_once 'footer.php'; // Loads footer​
+    ?>
+</body>
+</html>
